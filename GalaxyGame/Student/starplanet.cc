c283ef8511f6131fca38018c214dcd429dddb14a
Kucharova Lucie <lucie.kucharova@tieto.com>
2017-11-03 01:22:45 +0200

    Segment_fault in StarPlanet constructor eliminated

--- GalaxyGame/Student/starplanet.cc
+++ GalaxyGame/Student/starplanet.cc
@@ -4,16 +4,19 @@
 #include <QMap>
 #include <QList>
 #include <QGraphicsSceneMouseEvent>
+#include <vector>
+#include <string>
 
 StarPlanet::StarPlanet(unsigned id, uint64_t population, Common::Point point) :
     id_(id), population_(population), coordinates_(point)
 {
-    const char* planets[] =  {"Earth", "Jupiter", "Mars", "Mercury", "Moon", "Pluto", "Venus"};
-    const int cntPlanets = sizeof(planets)/sizeof(planets[0]);
-    int randomPlanet = Common::randomMinMax(0, cntPlanets);
-    QString planet = QString(":/images/images/planets/%1.png").arg(QString(planets[randomPlanet]).toLower());
+    // ToDo: load planets from file, store in separate object
+    std::vector<std::string> planets =  {"Earth", "Jupiter", "Mars", "Mercury", "Moon", "Pluto", "Venus"};
+    int randomPlanetIndex = Common::randomMinMax(0, planets.size()-1);
+    QString randomPlanetName = QString::fromStdString(planets.at(randomPlanetIndex));
+    QString planet = QString(":/images/images/planets/%1.png").arg(randomPlanetName.toLower());
 
-    name_ = planets[randomPlanet];
+    name_ = planets.at(randomPlanetIndex);
     goods_ = static_cast<GOODS_TYPE>(Common::randomMinMax(0, 5));
     economy_ = static_cast<Common::StarSystem::ECONOMY_TYPE>(Common::randomMinMax(0, 10));;
 
